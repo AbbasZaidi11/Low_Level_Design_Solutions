28d342b3045542370d8bb7487a0a13a129069bb4
Abbas Zaidi <abbasrazazaidi731@gmail.com>
2026-05-31 04:03:56 +0530

    atm lld

---
+++ ATM_Machine/src/main/java/org/example/ATMRoomComponents/UserBankAccount.java
@@ -0,0 +1,19 @@
+package org.example.ATMRoomComponents;
+
+
+public class UserBankAccount {
+
+    int balance;
+
+    public void withdrawalBalance(int amount) {
+        balance = balance - amount;
+    }
+
+    public int getBalance() {
+        return balance;
+    }
+
+    public void setBalance(int balance) {
+        this.balance = balance;
+    }
+}
---
+++ ATM_Machine/src/main/java/org/example/AmountWithdrawal/CashWithdrawProcessor.java
@@ -0,0 +1,21 @@
+package org.example.AmountWithdrawal;
+
+import org.example.ATMRoomComponents.ATM;
+
+public abstract class CashWithdrawProcessor {
+
+    CashWithdrawProcessor nextCashWithdrawalProcessor;
+
+    CashWithdrawProcessor(CashWithdrawProcessor cashWithdrawalProcessor) {
+
+        this.nextCashWithdrawalProcessor = cashWithdrawalProcessor;
+
+    }
+
+    public void withdraw(ATM atm, int remainingAmount) {
+
+        if (nextCashWithdrawalProcessor != null) {
+            nextCashWithdrawalProcessor.withdraw(atm, remainingAmount);
+        }
+    }
+}
